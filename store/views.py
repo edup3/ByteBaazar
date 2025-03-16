9b620b0ff3933d1146cda999d2f2a91591710a6b
edup3 <eduardopineros@hotmail.com>
2025-03-16 18:37:20 -0500

    refactor/deleted_UserContextMixin

--- store/views.py
+++ store/views.py
@@ -5,8 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import ListView, TemplateView, CreateView, DetailView
 from .forms import RegisterForm
-from .models import Product, Customer
-from .mixins import UserContextMixin
+from .models import Product, Customer, Review
 
 # Create your views here.
 
@@ -28,17 +27,30 @@ class LogoutCustomerView(LogoutView):
     success_url = reverse_lazy('home')
 
 
-class HomePageView(UserContextMixin, TemplateView):
+class HomePageView(TemplateView):
     template_name = 'store/index.html'
 
 
-class StorePageView(LoginRequiredMixin, UserContextMixin, ListView):
+class StorePageView(LoginRequiredMixin, ListView):
     login_url = '/login/'
     template_name = 'store/items.html'
     model = Product
     context_object_name = 'products'
 
 
-class ProductPageView(UserContextMixin, DetailView):
+class ProductPageView(DetailView):
     model = Product
     template_name = 'store/product.html'
+
+
+class CreateReviewView(LoginRequiredMixin, CreateView):
+    model = Review
+    template_name = 'store/review_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.product = Product.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('product_detail', kwargs={'pk': self.kwargs['pk']})
